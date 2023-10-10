91932774a359a8db9dab75360d2fcde9133cfb05
unknown <sj49@email.sc.edu>
2023-10-10 03:28:23 -0400

    Project part1

--- projects/part1/script.js
+++ projects/part1/script.js
@@ -2,12 +2,24 @@ const toggleNav = () => {
     document.getElementById("main-nav-items").classList.toggle("hidden");
 }
 
-const changeColors = () => {
-    const root = document.querySelector(":root");
-    root.style.setProperty("--primary", "coral");
-    root.style.setProperty("--primary", "blue");
+const signup = () => {
+    const addComment = document.getElementById("submit");
+
+    const commentP = document.createElement("p");
+    const commentHead = document.createElement("h3");
+
+    commentHead.appendChild(document.createTextNode(document.getElementById("name").value));
+
+    addComment.appendChild(commentHead);
+    addComment.appendChild(commentP);
+    
+    commentP.appendChild(document.createTextNode(document.getElementById("email").value));
+    commentP.appendChild(document.createTextNode(document.getElementById("pn").value));
+    commentP.appendChild(document.createElement("br"));
+    commentP.appendChild(document.createTextNode(document.getElementById("comment").value));
 }
 
 window.onload = () =>{
     document.getElementById("nav-toggle").onclick = toggleNav;
+    document.getElementById("submit-button").onclick = signup;
 }
