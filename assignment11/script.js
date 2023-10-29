23c3922a356ace413d425964b049ac6d4a7b7386
unknown <sj49@email.sc.edu>
2023-10-29 00:15:23 -0400

    sp 4

--- assignment11/script.js
+++ assignment11/script.js
@@ -4,7 +4,8 @@ const getMovies = async () => {
     try{
         const response = await fetch(url);
         return await response.json();
-    } catch(error){
+    } 
+    catch(error){
         console.log(error);
     }
 };
@@ -18,31 +19,36 @@ const showMovies = async () =>{
     });
 };
 
-const gteMovieItem = (movie) => {
-    let movieSection = document.createElement("section");
+
+//spelling error occured: 
+//constructor was gteMovieItem and not getMovieItem. 
+//developer tool was helpful in solving the problem.
+const getMovieItem = (movie) => {
+    let section = document.createElement("section");
     let span = document.createElement("span");
     const URL = "https://portiaportia.github.io/json/";
 
-    let movieImage = document.createElement("img");
+    let img = document.createElement("img");
     img.src = URL + movie.img;
     section.append(img);
 
-    let h3 = document.createElement("img");
-    h3.innerText = movie.name;
+    let h3 = document.createElement("h3");//had img instead of h3
+    h3.innerText = movie.title;
     span.append(h3);
     section.append(span);
 
+    //Something is wrong with p. It is only showing ${movie.whatever}
     let p = document.createElement("p");
     span.append(p);
-    p.innerHTML = "Director: ${movie.director}";
-    p.innerHTML = "Actors: ${movie.actors}";
-    p.innerHTML = "Year: ${movie.year}";
-    p.innerHTML = "Genre: ${movie.genres}";
-    p.innerHTML = "Description: ${movie.description}";
+    p.innerHTML = '<b>Director: </b>${movies.director}';
+    p.innerHTML += '<br><b> Actors: </b>${movies.actors}';
+    p.innerHTML += '<br><b>Year: </b> ${movies.year}';
+    p.innerHTML += '<br><b>Genres: </b> ${movies.genres}';
+    p.innerHTML += '<br><b>Description: </b>${movies.description}';
     section.append(span);
 
     section.classList.add("flexbox");
     return section;
-}
+};
 
 window.onload = () => showMovies();
