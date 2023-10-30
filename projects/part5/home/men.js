28ae2ad7f02468420ee77cdf0c58daa61e82e4d3
unknown <sj49@email.sc.edu>
2023-10-29 21:57:47 -0400

    Assignment 11

--- projects/part5/home/men.js
+++ projects/part5/home/men.js
@@ -10,7 +10,7 @@ const getProducts = async () => {
 };
 
 const showProducts = async () =>{
-    let products = await getMovies();
+    let products = await getProducts();
     let productsSection = document.getElementById("product-section");
 
     products.forEach((product) => {
@@ -20,18 +20,18 @@ const showProducts = async () =>{
 
 
 const getProductItem = (product) => {
-    const productSection = document.createElement("section");
+    const section = document.createElement("section");
     const span = document.createElement("span");
     const URL = "https://staceyjefferson22.github.io/json/";
 
     const img = document.createElement("img");
     img.src = URL + product.img;
-    productSection.append(img);
+    section.append(img);
 
     const h2 = document.createElement("h2");//had img instead of h3
     h2.innerText = product.name;
     span.append(h2);
-    productSection.append(span);
+    section.append(span);
 
     const p = document.createElement("p");
     span.append(p);
@@ -41,10 +41,10 @@ const getProductItem = (product) => {
     p.innerHTML += product.price;
     p.innerHTML += "<br><strong>Description: </strong>"
     p.innerHTML += product.price;
-    productSection.append(span);
+    section.append(span);
 
-    productSection.classList.add("flexbox");
-    return productSection;
+    section.classList.add("flexbox");
+    return section;
 };
 
 window.onload = () => showProducts();
