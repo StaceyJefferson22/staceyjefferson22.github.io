0095256f797e73d59347937ec6375fdda02cdc1a
unknown <sj49@email.sc.edu>
2023-11-03 13:03:11 -0400

    nodes

--- node/project1/server.js
+++ node/project1/server.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const app = express();
-app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(_dirname + "/index.html");
-});
-
-app.get("/api/cakes", (req, res) => {
-    const cakes = ["birthday cake", "red velvet", "chocolate cake", "cheese cake"];
-    res.send(cakes);
+app.get("/", (req, res) => {
+    res.send("hello world");
 });
 
 app.listen(3000, () => {
-    console.log("I'm listening");
+    console.log("I'm listening")
 });
